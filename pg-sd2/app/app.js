fd9086b1db19ac6b07dc88eeb14a1fd127914f05
pravallika0507 <pravallikanimmakayala22@gmail.com>
2023-01-05 02:05:42 +0000

    moving app.js html formate to pug template

--- pg-sd2/app/app.js
+++ pg-sd2/app/app.js
@@ -10,9 +10,45 @@ app.use(express.static("static"));
 // Get the functions in the db.js file to use
 const db = require('./services/db');
 
+// Use the Pug templating engine
+app.set('view engine', 'pug');
+app.set('views', './app/views');
+
 // Create a route for root - /
 app.get("/", function(req, res) {
-    res.send("Hello world!");
+    // Set up an array of data
+    var test_data = ['one', 'two', 'three', 'four'];
+    // Send the array through to the template as a variable called data
+    res.render("index", {'title':'My index page', 'heading':'My heading', 'data':test_data});
+});
+
+// // Task 2 display a formatted list of students
+// app.get("/all-students-formatted", function(req, res) {
+//     var sql = 'select * from Students';
+//     // As we are not inside an async function we cannot use await
+//     // So we use .then syntax to ensure that we wait until the 
+//     // promise returned by the async function is resolved before we proceed
+//     var output = '<table border="1px">';
+//     db.query(sql).then(results => {
+//         for (var row of results) {
+//             output += '<tr>';
+//             output += '<td>' + row.id + '</td>';
+//             output += '<td>' + '<a href="./single-student/' + row.id + '">' + row.name + '</a>' + '</td>';
+//             output += '</tr>'
+//         }
+//         output+= '</table>';
+//         res.send(output);
+//     });
+// });
+
+// Task 2 display a formatted list of students
+app.get("/all-students-formatted", function(req, res) {
+    var sql = 'select * from Students';
+    db.query(sql).then(results => {
+    	    // Send the results rows to the all-students template
+    	    // The rows will be in a variable called data
+        res.render('all-students', {data: results});
+    });
 });
 
 // Create a route for testing the db
